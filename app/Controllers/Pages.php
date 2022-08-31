6109904d4e32df33f3013ba02177175b2591e529
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2022-08-31 21:09:44 +0700

    Add files via upload

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function view($page = 'home')
+    {
+        //
+    }
+}
